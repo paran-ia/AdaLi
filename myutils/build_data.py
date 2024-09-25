9bee788cde577d298365e9e51c54da88ba1b4e28
paran <76645565+paran-ia@users.noreply.github.com>
2024-09-25 11:31:51 +0800

    Update build_data.py

--- myutils/build_data.py
+++ myutils/build_data.py
@@ -1,43 +1,52 @@
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
-from myutils.transform import Cutout
+from myutils.transform import Cutout, CIFAR10Policy
 import torch.utils.data as data
 import os
 import torch
 from data_process.cifar10_dvs import CIFAR10DVS
 from data_process.augmentation import ToPILImage, Resize, ToTensor
 from spikingjelly.datasets.dvs128_gesture import DVS128Gesture
+def build_cifar(dataset, data_dir, batch_size, num_workers=0, cutout=False, auto_aug=True):
 
+    aug = [transforms.RandomCrop(32, padding=4), transforms.RandomHorizontalFlip()]
+    if auto_aug:
+        aug.append(CIFAR10Policy)
 
-def build_cifar(dataset, data_dir, batch_size, num_workers=0):
+    aug.append(transforms.ToTensor())
+
+    if cutout:
+        aug.append(Cutout(n_holes=1, length=16))
+
     if dataset == 'CIFAR10':
         dataloader = datasets.CIFAR10
-        normalization_mean = (0.4914, 0.4822, 0.4465)
-        normalization_std = (0.2023, 0.1994, 0.2010)
-    elif dataset == 'CIFAR100':
-        dataloader = datasets.CIFAR100
-        normalization_mean = (0.5071, 0.4867, 0.4408)
-        normalization_std = (0.2675, 0.2565, 0.2761)
-    transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        Cutout(),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize(normalization_mean, normalization_std),
-    ])
+        aug.append(
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)))
+        transform_train = transforms.Compose(aug)
+        transform_test = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(
+                (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
 
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(normalization_mean, normalization_std),
-    ])
+    else:
+        dataloader = datasets.CIFAR100
+        aug.append(
+            transforms.Normalize(
+                (0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)),
+        )
+        transform_train = transforms.Compose(aug)
+        transform_test = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(
+                (0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)),
+        ])
 
     trainset = dataloader(root=data_dir, train=True, download=True, transform=transform_train)
     train_data_loader = data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-
     testset = dataloader(root=data_dir, train=False, download=False, transform=transform_test)
     test_data_loader = data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
     return train_data_loader, test_data_loader
-
 
 def build_tinyimagenet(data_dir, batch_size, num_workers=0):
     traindir = os.path.join(data_dir, 'train')
@@ -79,11 +88,11 @@ def build_cifar10dvs(data_dir, batch_size, frames_num, num_workers=0):
         ToTensor(),
     ])
 
-    trainset = CIFAR10DVS(data_dir, train=True, use_frame=True, frames_num=frames_num, split_by='number',
+    trainset = CIFAR10DVS(root = data_dir, train=True, use_frame=True, frames_num=frames_num, split_by='number',
                           normalization=None, transform=transform_train)
     train_data_loader = data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
 
-    testset = CIFAR10DVS(data_dir, train=False, use_frame=True, frames_num=frames_num, split_by='number',
+    testset = CIFAR10DVS(root =data_dir, train=False, use_frame=True, frames_num=frames_num, split_by='number',
                          normalization=None, transform=transform_test)
     test_data_loader = data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
     return train_data_loader, test_data_loader
